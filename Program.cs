a81fda7b070e7b34ab63c1598b1f5ff6ff2bc8d3
st-georgy <zhidkov.georg@gmail.com>
2022-12-28 20:03:29 +0300

    Init, added code files

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿char action = ' ';
+Console.Write("Введите число: ");
+var result = Convert.ToDouble(Console.ReadLine());
+while (action != '=')
+{
+    while (action != '='
+        && action != '+'
+        && action != '-'
+        && action != '/'
+        && action != '*')
+    {
+        Console.Write("Введите действие: ");
+        action = Convert.ToChar(Console.ReadLine());
+    }
+    if (action == '=') break;
+    Console.Write("Введите число: ");
+    var num = Convert.ToDouble(Console.ReadLine());
+    switch (action)
+    {
+        case '+':
+            result += num;
+            break;
+        case '-':
+            result -= num;
+            break;
+        case '*':
+            result *= num;
+            break;
+        case '/':
+            result /= num;
+            break;
+    }
+    action = ' ';
+}
+
+Console.WriteLine($"Ответ: {result}");
